4a3e50487a1602115b6c402a9fd783192c859423
Кирилл <67455038+Krutnik@users.noreply.github.com>
2021-01-15 18:56:41 +0300

    sper update

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ class CardCosplay {
     #_alt2;
     #_description;
     #_material;
+    #_onclick;
     #_parent;
 
-    constructor(name, src, alt, src2, alt2, description, material, parent) {
+    constructor(name, src, alt, src2, alt2, description, material, onclick, parent) {
         this.name = name;
         this.src = src;
         this.alt = alt;
@@ -16,6 +17,7 @@ class CardCosplay {
         this.alt2 = alt2;
         this.description = description;
         this.material = material;
+        this.onclick = onclick;
         this.parent = document.querySelector('.cosplay__project');
     }
 
@@ -68,6 +70,13 @@ class CardCosplay {
         this.#_material = materialValue;
     }
 
+    get onclick() {
+        return this.#_onclick;
+    }
+    set onclick(onclickValue) {
+        this.#_onclick = onclickValue;
+    }
+
     get parent() {
         return this.#_parent;
     }
@@ -96,8 +105,8 @@ class CardCosplay {
             <p>${this.description}</p>
             <h3>Material:</h3>
             <p>${this.material}</p>
-            <input class="card__description__btn" type="button" value="More" onclick="more()">
         </div>
+        <input class="card__btn" type="button" value="more" onclick="${this.onclick}">
         
         `;
         this.parent.append(elem);
@@ -111,53 +120,59 @@ let cards = [
         alt: 'lostvein',
         src2: 'img/lost.jpg',
         alt2: 'lostvein',
-        description: '...',
-        material: 'Wood'
+        description: 'Лоствейн - небольшой одноручный меч, изогнутый к острию и имеющий на своей гарде эмблему дракона. Серебристое лезвие меча можно отличить от тёмно- зелёного основания при помощи линии закалки, сквозь которую сделано пять отверстий одинакового диаметра.На боку основания есть вырезы в виде полумесяца.',
+        material: 'Меч выполнен полностью из дерева, красил эмалью, рисунок дракона карандашом выводил, рукоят взял зеленую ленту 20мм ленту',
+        onclick: 'more()'
     },
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        src2: 'img/lost.jpg',
-        alt2: 'lostvein',
-        description: '...',
-        material: 'Wood'
+        name: 'Gilgamesh',
+        src: 'img/gilgamesch.jpg',
+        alt: 'gilgamesh',
+        src2: 'img/gilgamesh-3.png',
+        alt2: 'gilgamesh',
+        description: 'Истинное имя Арчера — Гильгамеш, великий король, наполовину человек (отец — Лугальбанда, король города Урук), наполовину — бог (мать — богиня Нинсун). Правил городом-государством шумеров Урук, который был столицей древней Месопотамии. Он был на две трети богом и на треть человеком, и никто другой в мире не мог сравниться с ним. Он был деспотом, обладающим высокой божественностью и верившим в свою непобедимость. Он не просто выдуманная легенда, а, как говорят, действительно существовал и правил пять тысяч лет назад. Он был Королём Героев (英雄王, Eiyū Ō?, англ. King of Heroes), который обладал всеми вещами в мире. История о нём записана в старейшей эпической поэме, Эпосе о Гильгамеше, в которой Гильгамеш изображён героем, которому суждено стать королём и совершить великие подвиги, двигаясь навстречу своей судьбе и справляясь с проблемами вместе со своим лучшим другом Энкиду.',
+        material: 'Броня и меч из пвх, у меча имеется подсветка, парик заказной, также как и линзы, под броней обычный синий костюм',
+        onclick: 'regh()'
     },
-    // {
-    //     name: 'Lostvein',
-    //     src: 'img/lostvein.jpg',
-    //     alt: 'lostvein',
-    //     src2: 'img/lost.jpg',
-    //     alt2: 'lostvein',
-    //     description: '...',
-    //     material: 'Wood'
-    // },
-    // {
-    //     name: 'Lostvein',
-    //     src: 'img/lostvein.jpg',
-    //     alt: 'lostvein',
-    //     src2: 'img/lost.jpg',
-    //     alt2: 'lostvein',
-    //     description: '...',
-    //     material: 'Wood'
-    // },
-    // {
-    //     name: 'Lostvein',
-    //     src: 'img/lostvein.jpg',
-    //     alt: 'lostvein',
-    //     src2: 'img/lost.jpg',
-    //     alt2: 'lostvein',
-    //     description: '...',
-    //     material: 'Wood'
-    // },
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        src2: 'img/lost.jpg',
-        alt2: 'lostvein',
-        description: '...',
-        material: 'Wood'
+        name: 'Grove pugalo',
+        src: 'img/grove-pugalo.png',
+        alt: 'grove pugalo',
+        src2: 'img/pugalo2.jpg',
+        alt2: 'grove pugalo',
+        description: 'Пугало (англ. Scarecrow) — суперзлодей вселенной DC Comics. В детстве, когда Джонатан Крейн был ещё школьником, над ним издевались все его «знакомые». Однако Крейн обладал развитым умом. В студенческом возрасте он поступил в Готэмский университет на факультет психологии. Вскоре после отличной учёбы в университете Крейн стал профессором Аркхэмской клиники, однако его коллеги обнаружили профессора за опытами, проводимыми над обычными людьми. Его просто-напросто выгнали из Аркхэма. Движимый желанием отомстить за все свои обиды, обладающий навыками химии Крейн изобрёл галлюциогенный газ. Только Крейн начал осуществлять свой план, напав на бывших коллег профессоров, как на его пути встали Бэтмен и Робин. Профессора Крейна несколько раз сажали в тюрьму за преступления связанные с запугиванием людей.',
+        material: 'Пвх трубы и шланги, немного бинтов, иглы из спиц для вязания, состав жидкость под секртеом)))',
+        onclick: 'more2()'
+    },
+    {
+        name: 'Mjolnir',
+        src: 'img/molot.jpg',
+        alt: 'мjolnir',
+        src2: 'img/moltor.jpg',
+        alt2: 'мjolnir',
+        description: 'Мьёльнир (англ. Mjolnir) — боевой молот Тора. Тор носит молот Мьёльнир, выкованный из металла уру. Молот является практически неразрушимым, как земной металл адамантий, который покрывает скелет Росомахи, и сочетает в себе множество чар, наложенных на него Одином. Орудие может выдержать температуру солнца, взрывы, достаточные для уничтожения планет, и содержит в себе энергию, которой хватит, чтобы уничтожить целую галактику. Мьёльнир также даёт Тору множество способностей, например, управление фундаментальными силами вселенной, такими как электромагнитный спектр, гравитация и т.д .Но судя по обьяснениям Одина, Мльёнир лишь является инструментом, позволяющим контролировать внутреннюю мощь громовержцев.',
+        material: 'Выполнен полностью из дерева, верхушка с надписью сделана на пластике, на рукояти два слоя ткань, мягкая основа и кожзам',
+        onclick: 'more3()'
+    },
+    {
+        name: 'Thunder Harley Quinn',
+        src: 'img/thunder-hq.png',
+        alt: 'thunder Harley Quinn',
+        src2: 'img/hq.jpg',
+        alt2: 'thunder Harley Quinn',
+        description: 'Харли Квинн, ранее известная как доктор Харлин Ф. Квинзель, — бывший психиатр, ныне одна из противников Бэтмена, ставшая преступницей из-за любви к своему пациенту, психопату-убийце Джокеру. Впервые она появилась в сериале "Batman: The Animated Series", в эпизоде "Услуга Джокеру", где сыграла роль помощницы клоуна-принца. Её прозвище очень похоже по звучанию на слово «арлекин».',
+        material: 'Основа из дерева, молот полностью оклеен в пленку, добавлены элементы декора',
+        onclick: 'more4()'
+    },
+    {
+        name: 'Katana shinobu kocho',
+        src: 'img/katana.jpg',
+        alt: 'katana shinobu kocho',
+        src2: 'img/schinobu-kocho.jpeg',
+        alt2: 'katana shinobu kocho',
+        description: 'Цветок в форме с 4 лепестками; Лепесток имеет ядро цвета морской волны с оранжевой каймой.',
+        material: 'Лезвие из металла, рукоят состовная из дерева и пластика, краска эмаль и лента на рукоятку зеленая 50мм',
+        onclick: 'more5()'
     }
 ]
 
@@ -172,12 +187,13 @@ for (let i = 0; i < cards.length; i++) {
         cards[i]['alt2'],
         cards[i]['description'],
         cards[i]['material'],
+        cards[i]['onclick'],
         '.cosplay__project').createCardCosplay();
 } 
 
 
 let cart = document.querySelector('.card');
-let btnReset = document.querySelector('.input-style');
+let btnReset = document.querySelectorAll('.input-style');
 let children;
 
 function more() {
@@ -192,6 +208,25 @@ function more() {
 
     if(cart.classList.contains('big-card')) {
         cartInput.onclick = function() {
+            cart.classList.add('card');
+            cart.classList.remove('big-card');
+            children = cart.lastChild;
+            cart.removeChild(children);
+        }
+    }
+}
+function regh() {
+    cart.classList.remove('card');
+    cart.classList.add('big-card');
+
+    cartInput = document.createElement('input');
+    cartInput.classList.add('input-style');
+    cartInput.type = 'button';
+
+    cart.appendChild(cartInput);
+
+    if (cart.classList.contains('big-card')) {
+        cartInput.onclick = function () {
             cart.classList.add('card');
             cart.classList.remove('big-card');
             children = cart.lastChild;
@@ -258,8 +293,10 @@ s
         <h3 class="item__title">${this.names}</h3>
         <div class="item__description">
             <img class="item__description__image" src="${this.srcs}" alt="${this.alts}">
-            <h3 class="item__description__title">Description:</h3>
-            <p class="item__description__text">${this.descriptions}</p>
+            <div>
+                <h3 class="item__description__title">Description:</h3>
+                <p class="item__description__text">${this.descriptions}</p>
+            </div>
         </div>
         
         `;
@@ -269,28 +306,34 @@ s
 
 let items = [
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        description: '...'
+        name: 'Amon',
+        src: 'img/amon.jpg',
+        alt: 'amon',
+        description: '<p>Амон - из аниме Аватар: Легенда об Корре.</p> <p>Амон, ранее Ноатак - маг воды и маг крови из Северного Племени Воды, владеющий уникальной техникой блокировать магические способности других магов. Он является сыном в прошлом известного преступника Якона и старшим братом бывшего советника Тарлока. В 170 году ПГ, скрывая свою истинную личность и способности к магии, Амон возглавил революцию против магов.</p>'
     },
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        description: '...'
+        name: 'Bolter',
+        src: 'img/bolter.jpg',
+        alt: 'bolter',
+        description: '<p>Болтер (штурмовой пистолет космодесантника) из вселенной <span class="eng-text">Warhammer</span></p><p>Бoлтер — вид ствольного огнестрельного оружия, стреляющего саморазгоняющимися на траектории (в отличии от быстро теряющих скорость стабберных выстрелов) «болтами» — активно-реактивными снарядами, существующими во множестве вариаций и калибров, которые с огромной силой взрываются при попадании в цель, разрывая её на части изнутри.</p>'
     },
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        description: '...'
+        name: 'Sword Estarossa',
+        src: 'img/estarossa.jpeg',
+        alt: 'sword estarossa',
+        description: 'Эстаросса – элитный воин Клана Демонов, подчиняется непосредственно Королю Демонов, член Десяти Заповедей. На самом деле Эстароссы никогда не существовало.'
     },
     {
-        name: 'Lostvein',
-        src: 'img/lostvein.jpg',
-        alt: 'lostvein',
-        description: '...'
+        name: 'Fan Temari',
+        src: 'img/fan.jpeg',
+        alt: 'fan temari',
+        description: '<p>Гиганский веер Темари из аниме Наруто.</p> <p>Гигантский Складной Веер, которым владеет Темари, – это гигантский японский военный веер, который имеет три фиолетовых круга вдоль своего металлического корпуса, которые она называет "звездами". Сила создаваемых ею порывов ветра определяется количеством показанных звезд (одна из них-самая слабая, три-самые сильные).</p>'
+    },
+    {
+        name: 'Sword Helbram',
+        src: 'img/helbram.jpeg',
+        alt: 'sword helbram',
+        description: '<p>Хельбрам - фей, лучший друг Кинга из аниме Семь Смертных Грехов</p><p>Хельбрам - лучший друг Арлекина. Был убит самим Кингом 700 лет назад. После битвы в Лионесе мог появляться в виде духа перед Кингом, если тот надевал на голову шлем. Также Хельбрам может стать видимым для кого-то, если захочет. Ещё его могут видеть другие Короли Фей, как например, когда его видел Глоксиния, хотя Хельбрам не хотел ему показываться.</p>'
     }
 ]
 
@@ -307,6 +350,7 @@ for (let i = 0; i < items.length; i++) {
 
 
 let btnWood = document.getElementById('wood');
+
 let millingCutter = document.querySelector('.assistant__power-tool__milling-cutter');
 let enraver = document.querySelector('.assistant__power-tool__enraver');
 let lshm = document.querySelector('.assistant__power-tool__lshm');
@@ -315,6 +359,8 @@ let screwdriver = document.querySelector('.assistant__power-tool__screwdriver');
 let jigsaw = document.querySelector('.assistant__power-tool__jigsaw');
 let multiTool = document.querySelector('.assistant__power-tool__multi-tool');
 let circularSaw = document.querySelector('.assistant__power-tool__circular-saw');
+
+let textHidden = document.querySelector('.text-info');
 
 btnWood.onclick = function() {
     if (millingCutter.classList.contains('new-position') == false && 
@@ -327,6 +373,7 @@ btnWood.onclick = function() {
         circularSaw.classList.contains('new-position-eight') == false) {
 
         setTimeout(function() {
+            textHidden.classList.add('hidden');
             millingCutter.classList.add('new-position');
             enraver.classList.add('new-position-two');
             lshm.classList.add('new-position-three');
@@ -339,6 +386,7 @@ btnWood.onclick = function() {
     }
     else {
         setTimeout(function () {
+            textHidden.classList.remove('hidden');
             millingCutter.classList.remove('new-position');
             enraver.classList.remove('new-position-two');
             lshm.classList.remove('new-position-three');
@@ -347,6 +395,14 @@ btnWood.onclick = function() {
             jigsaw.classList.remove('new-position-six');
             multiTool.classList.remove('new-position-seven');
             circularSaw.classList.remove('new-position-eight');
+            millingCutter.classList.remove('test');
+            enraver.classList.remove('test');
+            lshm.classList.remove('test');
+            eshm.classList.remove('test');
+            screwdriver.classList.remove('test');
+            jigsaw.classList.remove('test');
+            multiTool.classList.remove('test');
+            circularSaw.classList.remove('test');
         }, 100);
     }
 }
@@ -510,6 +566,7 @@ btnPlastic.onclick = function () {
         technicalHairDryer.classList.contains('new-position-three') == false) {
 
         setTimeout(function () {
+            textHidden.classList.add('hidden');
             glueGun.classList.add('new-position-two');
             enraver.classList.add('new-position');
             multiTool.classList.add('new-position-four');
@@ -518,10 +575,15 @@ btnPlastic.onclick = function () {
     }
     else {
         setTimeout(function () {
+            textHidden.classList.remove('hidden');
             glueGun.classList.remove('new-position-two');
             enraver.classList.remove('new-position');
             multiTool.classList.remove('new-position-four');
             technicalHairDryer.classList.remove('new-position-three');
+            glueGun.classList.remove('test');
+            technicalHairDryer.classList.remove('test');
+            enraver.classList.remove('test');
+            multiTool.classList.remove('test');
         }, 100);
     }
 };
@@ -578,6 +640,7 @@ btnMetall.onclick = function () {
         compressor.classList.contains('new-position-three') == false) {
 
         setTimeout(function () {
+            textHidden.classList.add('hidden');
             ushm.classList.add('new-position-four');
             tocylinder.classList.add('new-position-two');
             compressor.classList.add('new-position-three');
@@ -587,11 +650,17 @@ btnMetall.onclick = function () {
     }
     else {
         setTimeout(function () {
+            textHidden.classList.remove('hidden');
             ushm.classList.remove('new-position-four');
             tocylinder.classList.remove('new-position-two');
             compressor.classList.remove('new-position-three');
             enraver.classList.remove('new-position-five');
             multiTool.classList.remove('new-position');
+            ushm.classList.remove('test');
+            tocylinder.classList.remove('test');
+            compressor.classList.remove('test');
+            enraver.classList.remove('test');
+            multiTool.classList.remove('test');
         }, 100);
     }
 }
